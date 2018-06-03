bbb9612cb6433a76206db382740397a5c387186c
yangjiahao <736420602@qq.com>
2018-06-03 16:32:34 +0800

    初始化

---
+++ src/pubG/components/production.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="container">
+    <div class="head">
+      <div class="join">
+        <div class="person"></div>
+        <div class="content">
+          <p class="name">用户名张三</p>
+          <p>今日投票剩余数 <span>3</span></p>
+        </div>
+        <div class="join-button">
+          <div class="finish-button"></div>
+        </div>
+      </div>
+      <div class="finish"></div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: 'navbar',
+  data () {
+    return {
+    }
+  },
+  props: {
+  },
+  methods: {
+  }
+}
+</script>
+<style scoped>
+.container{
+  background-color: #eeeeee;
+  border: 1px solid #eeeeee;
+  min-height: 100vh;
+}
+
+</style>
+
---
+++ src/pubG/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <section>
+    <div class="head">
+      <head-logo></head-logo>
+      <navbar v-model="selected" :list="navbarList"></navbar>
+    </div>
+    
+    <mt-tab-container v-model="selected">
+      <mt-tab-container-item id="1">
+        <home-page></home-page>
+      </mt-tab-container-item>
+      <mt-tab-container-item id="2">
+        <mt-cell v-for="n in 4" :title="'测试 ' + n" :key="n" />
+      </mt-tab-container-item>
+      <mt-tab-container-item id="3">
+        <production></production>
+      </mt-tab-container-item>
+      <mt-tab-container-item id="4">
+        <mt-cell v-for="n in 6" :title="'选项 ' + n"  :key="n"/>
+      </mt-tab-container-item>
+    </mt-tab-container>
+  </section>
+</template>
+
+<script>
+import HeadLogo from './components/headLogo'
+import Navbar from './components/navbar'
+import HomePage from './components/homePage'
+import Production from './components/production'
+export default {
+  name: 'page-navbar',
+  components:{
+    HeadLogo,
+    Navbar,
+    HomePage,
+    Production
+  },
+  data() {
+    return {
+      selected: '1',
+      navbarList: [{text: '首页', id: '1'},
+                {text: '赛事规则', id: '2'},
+                {text: '参赛作品', id: '3'},
+                {text: '获奖介绍', id: '4'}]
+    };
+  }
+};
+</script>
+<style scoped>
+.head{
+  background-color: #333333;
+}
+</style>
+
